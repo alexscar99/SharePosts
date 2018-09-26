04c78d6b8754bb11d5b420f2193f613f6c90ab4e
Alex Scarlett <alex.scarlett91@gmail.com>
2018-09-26 19:29:37 -0400

    Added Bootstrap CDN for CSS in the head and required the navbar file to display it

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -4,8 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="<?php print URLROOT; ?>/css/style.css">
         <title><?php print SITENAME; ?></title>
     </head>
 
     <body>
+        <?php require APPROOT . '/views/inc/navbar.php'; ?>
+        <div class="container">
