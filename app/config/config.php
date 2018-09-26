fc81ae55156583ca33e2af3902c28f7b92538a72
Alex Scarlett <alex.scarlett91@gmail.com>
2018-09-26 17:51:16 -0400

    Fixed typos and cleaned up switch statement for config

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,12 @@
 <?php
-    // Create switch statement to use either dev_config or prod_config
-    // files based off of environment
-
+    // Switch statement to use either dev_config or prod_config files based off of environment
     switch ($_SERVER['SERVER_NAME']) {
         case 'localhost':
             require_once 'dev_config.php';
             break;
-        // replace production-url with the server name for your production site
-        case 'production-url':
-            require_once 'production_config.php';
+        // replace prod url with the server name for prod site once ready for deploy
+        case 'prod-url':
+            require_once 'prod_config.php';
             break;
         default:
             // Error occurred
