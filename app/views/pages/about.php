50e0dc935ce798efaaf690ded0cd3a7b55620ffd
Alex Scarlett <alex.scarlett91@gmail.com>
2018-09-26 19:32:29 -0400

    Added description from about() in pages controller and displayed the app version via APPVERSION constant

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,3 +1,5 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
     <h1><?php print $data['title']; ?></h1>
+    <p><?php print $data['description']; ?></p>
+    <p>Version: <strong><?php print APPVERSION; ?></strong></p>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
